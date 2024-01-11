f9887d04b1457092ed6cd76de0b4370b3883aeb9
Leandro Morillo <leandro.m9601@gmail.com>
2024-01-11 11:47:18 -0500

    add children props

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -2,10 +2,21 @@ import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App() {
+
   return (
     <section className='TwCard'>
-        <TwitterFollowCard isFollowing={true} username="kikobeats" name="Kiko Beats"/>
-        <TwitterFollowCard isFollowing={false} username="midudev" name="Miguel Durán"/>
+        <TwitterFollowCard userName="kikobeats" isFollowing>
+          Kiko Beats  
+        </TwitterFollowCard>
+
+        <TwitterFollowCard userName="midudev" isFollowing={false}>
+          Miguel Durán
+        </TwitterFollowCard>
+
+        <TwitterFollowCard userName="lmorillo96" isFollowing={false}>
+          Leandro Morillo
+        </TwitterFollowCard>
+
     </section>
   );
 }
--- 00-hola-mundo/src/TwitterFollowCard.jsx
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,15 +1,15 @@
-export function TwitterFollowCard({ username, name, isFollowing }) {
+export function TwitterFollowCard({ children, userName = 'Unknown', isFollowing }) {
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
         <img
           className="tw-followCard-avatar"
           alt="TW Avatar"
-          src= {`https://unavatar.io/twitter/${username}`}     
+          src= {`https://unavatar.io/twitter/${userName}`}     
         />
         <div className="tw-followCard-info">
-          <strong>{name}</strong>
-          <span className="tw-followCard-userName">@{username}</span>
+          <strong>{children}</strong>
+          <span className="tw-followCard-userName">@{userName}</span>
         </div>
       </header>
 
